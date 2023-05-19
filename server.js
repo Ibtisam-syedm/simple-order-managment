523cb4744bbe9e8c5827eea3720255581202e252
ibtisam-syedm <m.ibtisam.syed@gmail.com>
2023-05-19 20:36:52 +0500

    Added local database for testing, configured env for production and development in database

--- server.js
+++ server.js
@@ -5,7 +5,17 @@ const port = process.env.PORT;
 const bodyParser = require("body-parser");
 const mongoose = require('mongoose');
 
-const uri = process.env.DB_URL;
+uri = ""
+if (process.env.NODE_ENV === 'production') {
+  
+} else if (process.env.NODE_ENV === 'development') {
+  if (process.env.DATABASE_ENV === 'local-database-testing') {
+    uri = process.env.DB_URL_LOCAL;
+  } else if (process.env.DATABASE_ENV === 'atlas-database-testing') {
+    uri = process.env.DB_URL_ATLAS;
+  }
+}
+
 mongoose.connect(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -19,119 +29,116 @@ db.once('open', () => {
 });
 
 const orderSchema = new mongoose.Schema({
-    userId: String,
-    productId: String,
-    quantity: Number,
-    completed: { type: Boolean, default: false },
-  });
-  
-  const Order = mongoose.model('Order', orderSchema);
-  
+  userId: String,
+  productId: String,
+  quantity: Number,
+  completed: { type: Boolean, default: false },
+});
 
+const Order = mongoose.model('Order', orderSchema);
 
-const orders = [];
 
 app.use(bodyParser.json());
 // Define routes and middleware here
 
 // Error handling middleware
 app.use((err, req, res, next) => {
-    if (err instanceof SyntaxError && err.status === 400 && 'body' in err) {
-      res.status(400).send('Invalid JSON payload');
-    } else if (err.status === 400 || err.status === 404) {
-      res.status(400).send(err.message);
-    } else {
-      console.error(err.stack);
-      res.status(500).send('Something went wrong!');
+  if (err instanceof SyntaxError && err.status === 400 && 'body' in err) {
+    res.status(400).send('Invalid JSON payload');
+  } else if (err.status === 400 || err.status === 404) {
+    res.status(400).send(err.message);
+  } else {
+    console.error(err.stack);
+    res.status(500).send('Something went wrong!');
+  }
+});
+
+app.put('/api/orders/:orderId/complete', async (req, res, next) => {
+  const orderId = req.params.orderId;
+
+  try {
+    const order = await Order.findByIdAndUpdate(
+      orderId,
+      { completed: true },
+      { new: true }
+    ).exec();
+
+    if (!order) {
+      return next(new Error('Order not found'));
     }
-  });
-  
-  app.put('/api/orders/:orderId/complete', async (req, res, next) => {
-    const orderId = req.params.orderId;
-  
-    try {
-      const order = await Order.findByIdAndUpdate(
-        orderId,
-        { completed: true },
-        { new: true }
-      ).exec();
-  
-      if (!order) {
-        return next(new Error('Order not found'));
-      }
-  
-      res.status(200).json(order);
-    } catch (err) {
+
+    res.status(200).json(order);
+  } catch (err) {
+    console.error(err);
+    next(new Error('Failed to complete order'));
+  }
+});
+
+
+
+app.post('/api/orders', async (req, res, next) => {
+  const { userId, productId, quantity } = req.body;
+
+  if (!userId || !productId || !quantity) {
+    const error = new Error('Missing required fields');
+    error.status = 400;
+    return next(error);
+  }
+
+  try {
+    const order = new Order({
+      userId,
+      productId,
+      quantity,
+    });
+
+    const savedOrder = await order.save();
+    res.status(201).json(savedOrder);
+  } catch (err) {
+    console.error(err);
+    next(new Error('Failed to save order'));
+  }
+});
+
+
+app.get('/api/orders/completed', (req, res, next) => {
+  Order.find({ completed: true })
+    .exec()
+    .then((completedOrders) => {
+      res.status(200).json(completedOrders);
+    })
+    .catch((err) => {
       console.error(err);
-      next(new Error('Failed to complete order'));
-    }
-  });
-  
-  
-  
-  app.post('/api/orders', async (req, res, next) => {
-    const { userId, productId, quantity } = req.body;
-  
-    if (!userId || !productId || !quantity) {
-      const error = new Error('Missing required fields');
-      error.status = 400;
-      return next(error);
-    }
-  
-    try {
-      const order = new Order({
-        userId,
-        productId,
-        quantity,
-      });
-  
-      const savedOrder = await order.save();
-      res.status(201).json(savedOrder);
-    } catch (err) {
+      next(new Error('Failed to fetch completed orders'));
+    });
+});
+
+app.get('/api/orders/uncompleted', (req, res, next) => {
+  Order.find({ completed: false })
+    .exec()
+    .then((uncompletedOrders) => {
+      res.status(200).json(uncompletedOrders);
+    })
+    .catch((err) => {
       console.error(err);
-      next(new Error('Failed to save order'));
-    }
-  });
-  
-  
-  app.get('/api/orders/completed', (req, res, next) => {
-    Order.find({ completed: true })
-      .exec()
-      .then((completedOrders) => {
-        res.status(200).json(completedOrders);
-      })
-      .catch((err) => {
-        console.error(err);
-        next(new Error('Failed to fetch completed orders'));
-      });
-  });
-  
-  app.get('/api/orders/uncompleted', (req, res, next) => {
-    Order.find({ completed: false })
-      .exec()
-      .then((uncompletedOrders) => {
-        res.status(200).json(uncompletedOrders);
-      })
-      .catch((err) => {
-        console.error(err);
-        next(new Error('Failed to fetch uncompleted orders'));
-      });
-  });
-  
-  app.get('/api/orders', (req, res, next) => {
-    Order.find({})
-      .exec()
-      .then((orders) => {
-        res.status(200).json(orders);
-      })
-      .catch((err) => {
-        console.error(err);
-        next(new Error('Failed to fetch orders'));
-      });
-  });
-  
-  
-  
+      next(new Error('Failed to fetch uncompleted orders'));
+    });
+});
+
+app.get('/api/orders', (req, res, next) => {
+  Order.find({})
+    .exec()
+    .then((orders) => {
+      res.status(200).json(orders);
+    })
+    .catch((err) => {
+      console.error(err);
+      next(new Error('Failed to fetch orders'));
+    });
+});
+
+
+
 // Start the server
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
